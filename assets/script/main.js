c9637d916a22c030fe1559d9c9421318812f0773
Kaan <kaan.kalmioglu@hva.nl>
2023-12-04 09:39:07 +0100

    begin aan interactie

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,30 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+let shake = document.querySelector('a:nth-of-type(1)')
+
+shake.addEventListener('click', shaker)
+shake.addEventListener('animationend', shaker)
+
+function shaker() {
+  shake.classList.toggle('shaker')
+} 
+
+let wobble = document.querySelector('a:nth-of-type(2)')
+
+wobble.addEventListener('click', wobbler)
+wobble.addEventListener('animationend', wobbler)
+
+function wobbler() {
+  wobble.classList.toggle('wobbler')
+} 
+
+let blink = document.querySelector('a:nth-of-type(3)')
+
+blink.addEventListener('click', blinker)
+blink.addEventListener('animationend', blinker)
+
+function blinker() {
+  blink.classList.toggle('blinker')
+} 
