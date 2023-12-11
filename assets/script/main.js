2ee457f498b78a354154af3d0654b1d6e8b7d390
Kaan <kaan.kalmioglu@hva.nl>
2023-12-11 10:52:53 +0100

    finished all 20 elements!
    and made the code even shorter!!

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,175 +1,108 @@
-let shake = document.querySelector('a:nth-of-type(1)') // selects the 1st a tag
+let shake = document.querySelector('a:nth-of-type(1)') // selects the 1st a tag WORKS
+let alarm = document.querySelector('a:nth-of-type(2)') // selects the 2nd a tag WORKS
+let bikelight = document.querySelector('a:nth-of-type(3)') // selects the 3rd a tag WORKS
+let luffy = document.querySelector('a:nth-of-type(4)') // selects the 4th a tag WORKS
+let trip = document.querySelector('a:nth-of-type(5)') // selects the 5th a tag WORKS
+let bounce = document.querySelector('a:nth-of-type(6)') // selects the 6th a tag WORKS
+let floom = document.querySelector('a:nth-of-type(7)') // selects the 7th a tag WORKS
+let woosh = document.querySelector('a:nth-of-type(8)') // selects the 8th a tag WORKS
+let bye = document.querySelector('a:nth-of-type(9)') // selects the 9th a tag WORKS
+let heya = document.querySelector('a:nth-of-type(10)') // selects the 10th a tag WORKS
+let abducted = document.querySelector('a:nth-of-type(11)') // selects the 11th a tag WORKS
+let interaction = document.querySelector('a:nth-of-type(12)') // selects the 12th a tag WORKS
+let roll = document.querySelector('a:nth-of-type(13)') // selects the 13th a tag WORKS
+let zoom = document.querySelector('a:nth-of-type(14)') // selects the 14th a tag WORKS
+let weee = document.querySelector('a:nth-of-type(15)') // selects the 15th a tag WORKS
+let grappler = document.querySelector('a:nth-of-type(16)') // selects the 16th a tag WORKS
+let switchteam = document.querySelector('a:nth-of-type(17)') // selects the 17th a tag WORKS
+let carti = document.querySelector('a:nth-of-type(18)') // selects the 18th a tag WORKS
+let travis = document.querySelector('a:nth-of-type(19)') // selects the 19th a tag WORKS
+let blubber = document.querySelector('a:nth-of-type(20)') // selects the 20th a tag WORKS
 
-console.log(shake) // shows in console
+//Shows in console if it picks the element up
+console.log(shake) // SHOWS
+console.log(alarm)  // SHOWS
+console.log(bikelight) // SHOWS
+console.log(luffy) // SHOWS
+console.log(trip) // SHOWS
+console.log(bounce) // SHOWS
+console.log(floom) // SHOWS
+console.log(woosh) // SHOWS
+console.log(bye) // SHOWS
+console.log(heya) // SHOWS
+console.log(abducted) // SHOWS
+console.log(interaction) // SHOWS
+console.log(roll) // SHOWS
+console.log(zoom) // SHOWS
+console.log(weee) // SHOWS
+console.log(grappler) // SHOWS
+console.log(switchteam) // SHOWS
+console.log(carti) // SHOWS
+console.log(travis) // SHOWS
+console.log(blubber) // SHOWS
 
-shake.addEventListener('click', shaker) // listens for click
-shake.addEventListener('animationend', shaker) // listens for animation end
+// listens for click
+shake.addEventListener('click', shaker) 
+alarm.addEventListener('click', wobbler)  
+bikelight.addEventListener('click', blinker) 
+luffy.addEventListener('click', gomugomu)  
+trip.addEventListener('click', lowbattery)
+bounce.addEventListener('click', bouncer)
+floom.addEventListener('click', rotate)
+woosh.addEventListener('click', disappear)
+bye.addEventListener('click', blurredout)
+heya.addEventListener('click', hello)
+abducted.addEventListener('click', rideOut)
+interaction.addEventListener('click', jumpHandler)
+roll.addEventListener('click', barrelroll)
+zoom.addEventListener('click', wave)
+weee.addEventListener('click', swirl)
+grappler.addEventListener('click', pulled)
+switchteam.addEventListener('click', michaeljackson)
+carti.addEventListener('click', what)
+travis.addEventListener('click', who)
+blubber.addEventListener('click', yuh)
 
-function shaker() {
-  shake.classList.toggle('shaker') // toggles the class shaker
-} 
+// listens for animation end
+alarm.addEventListener('animationend', wobbler) 
+shake.addEventListener('animationend', shaker)
+bikelight.addEventListener('animationend', blinker)
+luffy.addEventListener('animationend', gomugomu)
+trip.addEventListener('animationend', lowbattery)
+bounce.addEventListener('animationend', bouncer)
+floom.addEventListener('animationend', rotate)
+woosh.addEventListener('animationend', disappear)
+bye.addEventListener('animationend', blurredout)
+heya.addEventListener('animationend', hello)
+abducted.addEventListener('animationend', rideOut)
+interaction.addEventListener('animationend', jumpHandler)
+roll.addEventListener('animationend', barrelroll)
+zoom.addEventListener('animationend', wave)
+weee.addEventListener('animationend', swirl)
+grappler.addEventListener('animationend', pulled)
+switchteam.addEventListener('animationend', michaeljackson)
+carti.addEventListener('animationend', what)
+travis.addEventListener('animationend', who)
+blubber.addEventListener('animationend', yuh)
 
-let alarm = document.querySelector('a:nth-of-type(2)') // selects the 2nd a tag
-
-console.log(alarm)  // shows in console
-
-alarm.addEventListener('click', wobbler)  // listens for click
-alarm.addEventListener('animationend', wobbler) // listens for animation end
-
-function wobbler() {
-  alarm.classList.toggle('wobbler') // toggles the class wobbler
-} 
-
-let bikelight = document.querySelector('a:nth-of-type(3)') // selects the 3rd a tag
- 
-console.log(bikelight) // shows in console
-
-bikelight.addEventListener('click', blinker)  // listens for click
-bikelight.addEventListener('animationend', blinker) // listens for animation end
-
-function blinker() {
-  bikelight.classList.toggle('blinker') // toggles the class blinker
-} 
-
-let luffy = document.querySelector('a:nth-of-type(4)') // selects the 4th a tag
-
-console.log(luffy) // shows in console
-
-luffy.addEventListener('click', gomugomu)  // listens for click
-luffy.addEventListener('animationend', gomugomu) // listens for animation end
-
-function gomugomu() {
-  luffy.classList.toggle('gomugomu') // toggles the class rubberbanding
-}
-
-let trip = document.querySelector('a:nth-of-type(5)') // selects the 5th a tag
-
-console.log(trip) // shows in console
-
-trip.addEventListener('click', lowbattery) // listens for click
-trip.addEventListener('animationend', lowbattery) // listens for animation end
-
-function lowbattery() {
-  trip.classList.toggle('lowbattery') // toggles the class tripping
-}
-
-let bounce = document.querySelector('a:nth-of-type(6)') // selects the 6th a tag
-
-console.log(bounce) // shows in console
-
-bounce.addEventListener('click', bouncer) // listens for click
-bounce.addEventListener('animationend', bouncer) // listens for animation end
-
-function bouncer() {
-  bounce.classList.toggle('bouncer')  // toggles the class bouncer
-}
-
-let floom = document.querySelector('a:nth-of-type(7)') // selects the 7th a tag
-
-console.log(floom) // shows in console
-
-floom.addEventListener('click', rotate) // listens for click
-floom.addEventListener('animationend', rotate) // listens for animation end
-
-function rotate() { 
-  floom.classList.toggle('rotate') // toggles the class rotate
-} 
-
-let woosh = document.querySelector('a:nth-of-type(8)') // selects the 8th a tag
-
-console.log(woosh) // shows in console
-
-woosh.addEventListener('click', disappear) // listens for click
-woosh.addEventListener('animationend', disappear) // listens for animation end
-
-function disappear() {
-  woosh.classList.toggle('disappear') // toggles the class disappear
-}
-
-let bye = document.querySelector('a:nth-of-type(9)') // selects the 9th a tag
-
-console.log(bye) // shows in console
-
-bye.addEventListener('click', blurredout) // listens for click
-bye.addEventListener('animationend', blurredout) // listens for animation end
-
-function blurredout() {
-  bye.classList.toggle('blurredout') // toggles the class blurredout
-}
-
-let heya = document.querySelector('a:nth-of-type(10)') // selects the 10th a tag
-
-console.log(heya) // shows in console
-
-heya.addEventListener('click', hello) // listens for click
-heya.addEventListener('animationend', hello) // listens for animation end
-
-function hello() {
-  heya.classList.toggle('hello') // toggles the class hello
-}
-
-let abducted = document.querySelector('a:nth-of-type(11)') // selects the 11th a tag
-
-console.log(abducted) // shows in console
-
-abducted.addEventListener('click', rideOut) // listens for click
-abducted.addEventListener('animationend', rideOut) // listens for animation end
-
-function rideOut() {
-  abducted.classList.toggle('rideOut') // toggles the class rideOut
-}
-
-let interaction = document.querySelector('a:nth-of-type(12)') // selects the 12th a tag
-
-console.log(interaction)  // shows in console
-
-interaction.addEventListener('click', jumpHandler) // listens for click
-interaction.addEventListener('animationend', jumpHandler) // listens for animation end
-
-function jumpHandler() { 
-  interaction.classList.toggle('jump') // toggles the class jump
-}
-
-let roll = document.querySelector('a:nth-of-type(13)') // selects the 13th a tag
-
-console.log(roll) // shows in console
-
-roll.addEventListener('click', barrelroll) // listens for click
-roll.addEventListener('animationend', barrelroll) // listens for animation end
-
-function barrelroll() {
-  roll.classList.toggle('barrelroll') // toggles the class barrelroll
-}
-
-let zoom = document.querySelector('a:nth-of-type(14)') // selects the 14th a tag
-
-console.log(zoom) // shows in console
-
-zoom.addEventListener('click', wave) // listens for click
-zoom.addEventListener('animationend', wave) // listens for animation end
-
-function wave() {
-  zoom.classList.toggle('wave') // toggles the class wave
-}
-
-let weee = document.querySelector('a:nth-of-type(15)') // selects the 15th a tag
-
-console.log(weee) // shows in console
-
-weee.addEventListener('click', swirl) // listens for click
-weee.addEventListener('animationend', swirl) // listens for animation end
-
-function swirl() {
-  weee.classList.toggle('swirl') // toggles the class swirl
-}
-
-let grappler = document.querySelector('a:nth-of-type(16)') // selects the 16th a tag
-
-console.log(grappler) // shows in console
-
-grappler.addEventListener('click', pulled) // listens for click
-grappler.addEventListener('animationend', pulled) // listens for animation end
-
-function pulled() {
-  grappler.classList.toggle('pulled') // toggles the class pulled
-}
+//Functions that toggle the class on and off for the animations
+function shaker() { shake.classList.toggle('shaker') }
+function wobbler() { alarm.classList.toggle('wobbler') } 
+function blinker() { bikelight.classList.toggle('blinker') } 
+function gomugomu() { luffy.classList.toggle('gomugomu') }
+function lowbattery() { trip.classList.toggle('lowbattery') }
+function bouncer() { bounce.classList.toggle('bouncer') }
+function rotate() { floom.classList.toggle('rotate') }
+function disappear() { woosh.classList.toggle('disappear') }
+function blurredout() { bye.classList.toggle('blurredout') }
+function hello() { heya.classList.toggle('hello') }
+function rideOut() {abducted.classList.toggle('rideOut') }
+function jumpHandler() { interaction.classList.toggle('jump') }
+function barrelroll() { roll.classList.toggle('barrelroll') }
+function wave() { zoom.classList.toggle('wave') }
+function swirl() { weee.classList.toggle('swirl') }
+function pulled() { grappler.classList.toggle('pulled')  }
+function michaeljackson() { switchteam.classList.toggle('michaeljackson') }
+function what() { carti.classList.toggle('what') }
+function who() { travis.classList.toggle('who') }
+function yuh() { blubber.classList.toggle('yuh') }
